856f6ed613e0a040bffe0f29cbd5f1d50ea0b532
V.H.T <20156510@student.hust.edu.vn>
2017-11-17 15:00:25 +0700

    Xay dung tooltip va key-shorcut voi command. Gap van de trong Custom Routed Comman

--- Test6/Test6/MainWindow.xaml.cs
+++ Test6/Test6/MainWindow.xaml.cs
@@ -23,68 +23,49 @@ namespace Test6
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class MyApplicationCommands
+    {
+        public static RoutedUICommand ExitApp = new RoutedUICommand("Exit the application", "ExitApp", typeof(MyApplicationCommands));
+    }
+
     public partial class MainWindow : RibbonWindow
     {
+        public static string pathFile = null;  //bien global luu tru duong dan file
         public MainWindow()
         {
             InitializeComponent();
         }
-
+       
         //Cac chuc nang trong ApplicationMenu
-        public void appNew_Click(object sender, EventArgs e)
-        {
-            richTextBox1.Document.Blocks.Clear();
-        }
-
-        public void appOpen_Click(object sender, EventArgs e)
+        private void OpenCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             System.Windows.Forms.OpenFileDialog openFile = new System.Windows.Forms.OpenFileDialog();
             openFile.Filter = "text files (*.txt)|*.txt|All Files (*.*)|*.*";
 
-            /* cach lam cho OpenFileDialog cua Microsoft.Win32
-            Nullable<bool> result = openFile.ShowDialog();
-            if (result == true)
-            {
-                TextRange content = new TextRange(richTextBox1.Document.ContentStart, richTextBox1.Document.ContentEnd);
-                FileStream fStream = new FileStream(openFile.FileName, FileMode.OpenOrCreate);
-                content.Load(fStream, System.Windows.DataFormats.Text);
-                fStream.Close();
-
-                this.Title = openFile.FileName;
-            }*/
-
-            /*
             if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                string fileName = openFile.FileName;
-                //FileStream file = new FileStream(fileName, FileMode.Open);
-               
-                
-                    string content = File.ReadAllText(openFile.FileName);
-                    richTextBox1.Document.Blocks.Clear();
-                    richTextBox1.Document.Blocks.Add(new Paragraph(new Run(content)));
-                
-                //file.Close();
-            }    */
-
-            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                Stream s = File.Open(openFile.FileName, FileMode.Open);
-                StreamReader sRead = new StreamReader(s);
+                Stream stream = File.Open(openFile.FileName, FileMode.Open);
+                StreamReader sRead = new StreamReader(stream);
 
                 string content = sRead.ReadToEnd();
                 richTextBox1.Document.Blocks.Clear();
                 richTextBox1.Document.Blocks.Add(new Paragraph(new Run(content)));
 
                 sRead.Close();
-                s.Close();
+                stream.Close();
 
-                this.Title = openFile.FileName;
+                //chuyen ten window sang ten file; pathFile nhan duong dan truc tiep den file
+                this.Title = openFile.SafeFileName;
+                pathFile = openFile.FileName;
             }
-            
         }
 
-        public void saveAsFunc()
+        private void NewCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            richTextBox1.Document.Blocks.Clear();
+        }
+
+        private void saveAsFunc()
         {
             System.Windows.Forms.SaveFileDialog saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs";
@@ -101,15 +82,15 @@ namespace Test6
             }
         }
 
-        public void appSave_Click(object sender, EventArgs e)
+        private void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            if (!File.Exists(this.Title))
+            if (!File.Exists(pathFile))
             {
                 saveAsFunc();
             }
             else
             {
-                Stream s = File.Open(this.Title, FileMode.OpenOrCreate);
+                Stream s = File.Open(pathFile, FileMode.OpenOrCreate);
                 StreamWriter sWrite = new StreamWriter(s);
 
                 string content = new TextRange(richTextBox1.Document.ContentStart, richTextBox1.Document.ContentEnd).Text;
@@ -119,35 +100,12 @@ namespace Test6
             }
         }
 
-        public void appSaveAs_Click(object sender, EventArgs e)
+        private void SaveAsCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             saveAsFunc();
         }
-
-        public void appExit_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-
     
         //Cac chuc nang trong QuickAccessToolbar
-        public void saveButton1_Click(object sender, EventArgs e)
-        {
-            if (!File.Exists(this.Title))
-            {
-                saveAsFunc();
-            }
-            else
-            {
-                Stream s = File.Open(this.Title, FileMode.OpenOrCreate);
-                StreamWriter sWrite = new StreamWriter(s);
-
-                string content = new TextRange(richTextBox1.Document.ContentStart, richTextBox1.Document.ContentEnd).Text;
-                sWrite.Write(content);
-                sWrite.Close();
-                s.Close();
-            }
-        }
 
         public void undoButton1_Click (object sender, EventArgs e)
         {
@@ -166,7 +124,7 @@ namespace Test6
             undoButton2.IsEnabled = true;
             redoButton1.IsEnabled = false;
             redoButton2.IsEnabled = false;
-        }
+        } 
 
         //Cac chuc nang trong RibbonTab GENERAL
 
@@ -175,11 +133,7 @@ namespace Test6
         {
             richTextBox1.Paste();
         }
-
-        public void cutButton_Click (object sender, EventArgs e)
-        {
-            richTextBox1.Cut();
-        }
+       
 
         public void copyButton_Click (object sender, EventArgs e)
         {
@@ -224,6 +178,18 @@ namespace Test6
             richTextBox1.Selection.ApplyPropertyValue(Run.FontStyleProperty, FontStyles.Italic);
         }
 
+        private void underlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            // thay doi thuoc tinh TextDecorations cua doan text duoc chon thanh Underline
+            richTextBox1.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+        }
+
+        private void strikeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // thay doi thuoc tinh TextDecorations cua doan text duoc chon thanh Strikethrough
+            richTextBox1.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Strikethrough);
+        }
+
         //Cac chuc nang trong RibbonTab ADVANCED
         public void speechObject_Click(object sender, EventArgs e)
         {
@@ -250,14 +216,14 @@ namespace Test6
                 undoButton1.IsEnabled = true;
                 undoButton2.IsEnabled = true;
 
-                cutButton.IsEnabled = true;
+                
                 copyButton.IsEnabled = true;
                 selectAllButton.IsEnabled = true;
 
                 boldButton.IsEnabled = true;
                 italicButton.IsEnabled = true;
                 underlineButton.IsEnabled = true;
-                spikeButton.IsEnabled = true;
+                strikeButton.IsEnabled = true;
                 deleteButton.IsEnabled = true;
 
                 speechObject.IsEnabled = true;
@@ -266,21 +232,20 @@ namespace Test6
             {
                 undoButton1.IsEnabled = false;
                 undoButton2.IsEnabled = false;
-
-                cutButton.IsEnabled = false;
+                
                 copyButton.IsEnabled = false;
                 selectAllButton.IsEnabled = false;
 
                 boldButton.IsEnabled = false;
                 italicButton.IsEnabled = false;
                 underlineButton.IsEnabled = false;
-                spikeButton.IsEnabled = false;
+                strikeButton.IsEnabled = false;
                 deleteButton.IsEnabled = false;
 
                 speechObject.IsEnabled = false;
             }
         }
 
-       
+        
     }
 }
